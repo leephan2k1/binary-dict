cc490b76e3554f0e91d614689329d3a7aaae381d
lee phan <pttl2k1@gmail.com>
2022-01-27 02:01:07 +0700

    css add word

--- src/renderer/app/index.js
+++ src/renderer/app/index.js
@@ -1,8 +1,59 @@
+const menuItemsDOM = document.querySelectorAll(".menu-item");
+const framesDOM = document.querySelectorAll(".frame");
+
+const resultFrameDOM = document.querySelector("#result-frame");
+const wordListDOM = document.querySelector("#wordList");
+const filterDOM = document.querySelector("#filter");
+const searchFrameDOM = document.querySelector("#search-frame");
+const addWordFrameDOM = document.querySelector("#add-word-frame");
+
 const app = {
+  activeNavbar: function () {
+    menuItemsDOM.forEach((menuItem) => {
+      menuItem.addEventListener("click", () => {
+        //remove all active
+        menuItemsDOM.forEach((e) => {
+          e.classList.remove("active");
+        });
+        //hidden all frame
+        framesDOM.forEach((e) => {
+          e.classList.add("hidden");
+        });
+        //active current item
+        menuItem.classList.add("active");
+        //display current frame match with item
+        this.activeFrame();
+      });
+    });
+  },
 
+  activeFrame: function () {
+    let indexFrame;
+    menuItemsDOM.forEach((item, index) => {
+      if (item.classList.contains("active")) {
+        indexFrame = index;
+        return;
+      }
+    });
+    switch (indexFrame) {
+      case 0:
+        //display search + result search
+        resultFrameDOM.classList.remove("hidden");
+        searchFrameDOM.classList.remove("hidden", "h-[20%]");
+        searchFrameDOM.classList.add("h-[30%]");
+        break;
+      case 1:
+        //display search + filter + word list
+        searchFrameDOM.classList.remove("hidden", "h-[30%]");
+        searchFrameDOM.classList.add("h-[20%]");
+        filterDOM.classList.remove("hidden");
+        wordListDOM.classList.remove("hidden");
+        break;
+      case 2:
+        addWordFrameDOM.classList.remove("hidden");
+        break;
+    }
+  },
+};
 
-
-
-
-    
-}
+app.activeNavbar();
