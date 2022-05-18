31d37851aace8575aba9e798fc774b50579d2f74
leephan <pttl2k1@gmail.com>
2022-05-18 10:18:49 +0700

    test: 💍 blackbox testing binary search tree

--- testing/BinarySearchTree.test.js
+++ testing/BinarySearchTree.test.js
@@ -7,20 +7,20 @@ const root = new BinarySearchTree((objWordA, objWordB) => {
   return objWordA?.word < objWordB?.word ? -1 : 1;
 });
 const initial = () => {
-  // root.insert({ word: "dog" });
-  // root.insert({ word: "manual" });
-  // root.insert({ word: "binary" });
-  // root.insert({ word: "search" });
-  // root.insert({ word: "risk" });
-  // root.insert({ word: "tree" });
-  // root.insert({ word: "testing" });
-  root.insert({ word: "a" });
-  root.insert({ word: "a b c" });
-  root.insert({ word: "a font" });
-  root.insert({ word: "a fortiori" });
-  root.insert({ word: "a la carte" });
+  root.insert({ word: "dog" });
+  root.insert({ word: "manual" });
+  root.insert({ word: "binary" });
+  root.insert({ word: "search" });
+  root.insert({ word: "risk" });
+  root.insert({ word: "tree" });
+  root.insert({ word: "testing" });
+  // root.insert({ word: "a" });
+  // root.insert({ word: "a b c" });
+  // root.insert({ word: "a font" });
+  // root.insert({ word: "a fortiori" });
+  // root.insert({ word: "a la carte" });
 };
-initial();
+// initial();
 /*
                 dog
                /   \
@@ -36,61 +36,79 @@ initial();
 
 */
 
-//Mong đợi: (word: )  binary, dog, manual, risk, search, testing, tree
-console.log("test id 1: ", root.toString());
+beforeEach(() => {
+  return initial();
+});
 
-//Mong đợi false:
-console.log("test id 2: ", root.contains({ word: "test" }));
-//Mong đợi true:
-console.log("test id 3: ", root.contains({ word: "dog" }));
+test("TC1: kiem tra duyet trung tu", () => {
+  expect(root.toString()).toBe(
+    '{"word":"binary"},{"word":"dog"},{"word":"manual"},{"word":"risk"},{"word":"search"},{"word":"testing"},{"word":"tree"}'
+  );
+});
 
-root.remove({word: "tree"});
-/*
-                dog
-               /   \
-           binary  manual
-          /   \     /   \
-        null null null search     
-                        /   \ 
-                     risk  testing
-                           /    \
-                         null   null     
+test("TC2: kiem tra method contains", () => {
+  expect(root.contains({ word: "test" })).toBe(false);
+});
 
-*/
-//Mong đợi: (word: )  binary, dog, manual, risk, search, testing
-console.log("test id 4: ", root.toString());
+test("TC3: kiem tra method contains", () => {
+  expect(root.contains({ word: "dog" })).toBe(true);
+});
 
-initial();
-root.remove({word: "search"});
-/*
-                dog
-               /   \
-           binary  manual
-          /   \     /   \
-        null null null testing     
-                        /   \ 
-                     risk   tree
-                           /    \
-                         null   null        
-*/
-//Mong đợi: (word: )  binary, dog, manual, risk, testing, tree
-console.log("test id 5: ", root.toString());
+test("TC4: kiem tra method remove", () => {
+  root.remove({ word: "tree" });
+  expect(root.toString()).toBe(
+    '{"word":"binary"},{"word":"dog"},{"word":"manual"},{"word":"risk"},{"word":"search"},{"word":"testing"}'
+  );
+});
+// /*
+//                 dog
+//                /   \
+//            binary  manual
+//           /   \     /   \
+//         null null null search
+//                         /   \
+//                      risk  testing
+//                            /    \
+//                          null   null
 
-//TEST SEARCH
-initial();
-/*
-                dog
-               /   \
-           binary  manual
-          /   \     /   \
-        null null null search     
-                        /   \ 
-                     risk   tree
-                           /    \
-                       testing   null
-                       /     \
-                     null   null          
+test("TC5: kiem tra method remove", () => {
+  root.remove({ word: "search" });
+  expect(root.toString()).toBe(
+    '{"word":"binary"},{"word":"dog"},{"word":"manual"},{"word":"risk"},{"word":"testing"},{"word":"tree"}'
+  );
+});
 
-*/
-const result = root.search({word: "hihi"})
-console.log(result);
+// /*
+//                 dog
+//                /   \
+//            binary  manual
+//           /   \     /   \
+//         null null null testing
+//                         /   \
+//                      risk   tree
+//                            /    \
+//                          null   null
+// */
+
+// /*
+//                 dog
+//                /   \
+//            binary  manual
+//           /   \     /   \
+//         null null null search
+//                         /   \
+//                      risk   tree
+//                            /    \
+//                        testing   null
+//                        /     \
+//                      null   null
+
+test("TC6: kiem tra method search", () => {
+  expect(root.search({ word: "hihi" })).toBe(null);
+});
+
+test("TC7: kiem tra method search", () => {
+  expect(JSON.stringify(root.search({ word: "risk" }).value)).toBe(
+    '{"word":"risk"}'
+  );
+});
